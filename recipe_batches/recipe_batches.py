abeb5e4ab4ba6c849eb68341992f283d90d6d401
zoverlvx <zoverhulser@gmail.com>
2019-12-12 21:16:54 +0000

    Passes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,22 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    recipe_amounts= list(recipe.values())
+    amounts_of_ingredients = list(ingredients.values())
+    if (len(recipe_amounts) > len(amounts_of_ingredients)):
+        return 0
+    for item in recipe:
+        if item not in ingredients:
+            return 0
+    length = len(recipe_amounts)
+    amounts = []
+    for i in range(length):
+        amount = amounts_of_ingredients[i] // recipe_amounts[i]
+        if(amount == 0):
+            return 0
+        amounts.append(amount)
+    amounts.sort()
+    return amounts[0]
 
 
 if __name__ == '__main__':
