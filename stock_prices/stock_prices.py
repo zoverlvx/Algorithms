b2f3fc0329456ba9ef769c2099e22a740f7165ce
zoverlvx <zoverhulser@gmail.com>
2019-12-12 01:31:45 +0000

    It's a start

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,26 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    # creates new copy of original order of prices
+    original = [] + prices
+    # sorts prices in ascending order
+    prices.sort()
+    # points to the highest value price
+    lastIndex = len(prices) - 1
+    highest = prices[lastIndex]
 
+    # find the index of the highest value in the original order
+    indexOfOriginal = original.index(highest)
+    lowest = highest
+    length = len(prices) - 1
+    for i in range(length):
+        if i < indexOfOriginal:
+            if original[i] < lowest:
+                lowest = original[i]
+                print(lowest)
+
+
+    return highest - lowest
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
