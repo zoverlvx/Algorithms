9d3565459c2b398fcdddf5f9dc743c3486d9cdd1
zoverlvx <zoverhulser@gmail.com>
2020-01-23 17:06:43 -0500

    Xu's code

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,14 +6,21 @@ from functools import lru_cache
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
 @lru_cache(maxsize = 1000)
 def eating_cookies(n, cache=None):
-    if n <= 1:
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    if n == 2:
-        return 2
+	elif cache and cache[n] > 0:
+		return cache[n]
     else:
-        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+		if not cache:
+			cache = {i: 0 for i in range(n+1)}
+		cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+		return cache[n]
+        
 
 
 
