4bb48c79e5777cd0c7570b185d46f0542d7701eb
zoverlvx <zoverhulser@gmail.com>
2020-01-23 18:08:49 -0500

    Passes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,6 +7,7 @@ from functools import lru_cache
 # a solution that is more efficient than the naive 
 # recursive solution
 
+"""
 @lru_cache(maxsize = 1000)
 def eating_cookies(n, cache=None):
     if n < 0:
@@ -20,10 +21,21 @@ def eating_cookies(n, cache=None):
 			cache = {i: 0 for i in range(n+1)}
 		cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
 		return cache[n]
+"""
+
+def eating_cookies(n, cache={}):
+	# base cases
+	if n == 0:
+		return 1
+	elif n < 3:
+		return n
+	elif n == 3:
+		return 4
+
+	elif n not in cache:
+		cache[n] = eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+	return cache[n]
         
-
-
-
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
